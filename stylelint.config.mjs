d24014862e14d469a7dd1c0451d0332c4ee127f4
mugbee <mugbee2025@gmail.com>
2026-01-01 01:24:01 +0900

    chore: 📦 Stylelintを追加する
    CSS・SCSSのチェックを行うために導入する

---
+++ stylelint.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
+    "stylelint-config-recess-order",
+    "stylelint-config-html",
+    "stylelint-config-html/astro",
+    "stylelint-config-prettier-scss",
+  ],
+
+  rules: {
+    "color-function-alias-notation": "with-alpha",
+    "color-function-notation": "modern",
+
+    "selector-class-pattern": [
+      "^[a-z][a-z0-9-]*(?:__[a-z0-9-]+)?(?:--[a-z0-9-]+)?$",
+      {
+        message:
+          "クラスセレクターは BEM 記法（block__element--modifier）に従ってください。",
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: ["**/*.astro"],
+      rules: {
+        "no-descending-specificity": null,
+      },
+    },
+  ],
+};
