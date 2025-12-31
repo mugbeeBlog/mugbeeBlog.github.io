7420f077afd5f47b9d730d747d815a9edb8878e2
mugbee <mugbee2025@gmail.com>
2025-12-31 23:20:14 +0900

    chore: 📦 WSL2環境向けに Viteの watch設定を追加する

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://mugbee2026.github.io",
+  vite: {
+    server: {
+      watch: {
+        usePolling: true, // WSL2環境での変更検知安定化
+      },
+    },
+  },
 });
