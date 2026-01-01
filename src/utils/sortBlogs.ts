48bb3cdc0535d99c3135ef6e20e9eca2bc8c9ffd
mugbee <mugbee2025@gmail.com>
2026-01-02 00:10:13 +0900

    refactor: 🧩 互換性を考慮したメソッドに変更する
    Astrov5の最小要件がNode.js v18、toSorted()はv20から。

--- src/utils/sortBlogs.ts
+++ src/utils/sortBlogs.ts
@@ -3,7 +3,7 @@ import type { CollectionEntry } from 'astro:content';
 export const sortBlogsByDate = (
   blogs: CollectionEntry<'blogs'>[]
 ): CollectionEntry<'blogs'>[] =>
-  blogs.toSorted(
+  [...blogs].sort(
     (a, b) =>
       b.data.publishedAt.getTime() - a.data.publishedAt.getTime() ||
       a.data.slug.localeCompare(b.data.slug)
