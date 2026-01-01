a0fb1e920a482ee032f56b16ee0e6ae54d981130
mugbee <mugbee2025@gmail.com>
2026-01-01 23:44:53 +0900

    refactor: 🧩 破壊的メソッドを削除してスプレッド構文に変更する
    toSorted()はES2023で追加されたメソッド

--- src/utils/sortBlogs.ts
+++ src/utils/sortBlogs.ts
@@ -3,7 +3,7 @@ import type { CollectionEntry } from 'astro:content';
 export const sortBlogsByDate = (
   blogs: CollectionEntry<'blogs'>[]
 ): CollectionEntry<'blogs'>[] =>
-  blogs.sort((a, b) => {
+  blogs.toSorted((a, b) => {
     const dateA = a.data.publishedAt?.getTime() ?? 0;
     const dateB = b.data.publishedAt?.getTime() ?? 0;
     return dateB - dateA || a.data.slug.localeCompare(b.data.slug);
