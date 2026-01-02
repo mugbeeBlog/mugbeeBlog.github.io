77e6cf0b8c1c9604c499da21ad767fde03828d02
mugbee <mugbee2025@gmail.com>
2026-01-02 19:51:32 +0900

    chore: 📦 RSSフィード生成を追加する

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,23 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { site } from '../data/constants';
+
+export async function GET() {
+  const posts = (await getCollection('blogs')).sort(
+    (a, b) => b.data.publishedAt - a.data.publishedAt
+  );
+
+  return rss({
+    title: site.title,
+    description: site.description,
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      link: `/${post.data.slug}/`,
+      pubDate: post.data.publishedAt,
+      description:
+        post.data.description ??
+        post.data.content.replace(/<[^>]*>?/gm, '').slice(0, 120),
+    })),
+  });
+}
